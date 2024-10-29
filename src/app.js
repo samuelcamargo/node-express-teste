466dca72b99855d33a5d39e8d6a6000af3f78c16
samuel camargo <samuelcamargo1@gmail.com>
2024-10-29 08:31:57 -0300

    subida do projeto base só node.js e começo do express

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const livros = [
+ {id: 1, titulo :"Senhor dos aneis"},
+ {id: 2, titulo :"O Hobbit"}
+]
+
+app.get("/",(req,res)=>{
+ res.send(200,'Curso de Node.js');
+});
+
+app.get("/livros",(req,res)=>{
+ res.status(200).json(livros);
+});
+
+app.post("/livros",(req,res)=>{
+ livros.push(req.body);
+ res.status(201).send("livros Cadastrado com sucesso");
+});
+
+export default app;
