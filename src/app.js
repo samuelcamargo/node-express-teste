6843bcd74e2dbfa228d9c0d0be62bb48d34f3474
samuel camargo <samuelcamargo1@gmail.com>
2024-10-29 10:58:29 -0300

    adicionado get put delit post etc

--- src/app.js
+++ src/app.js
@@ -16,9 +16,33 @@ app.get("/livros",(req,res)=>{
  res.status(200).json(livros);
 });
 
+app.get("/livros/:id",(req,res)=>{
+ const index = buscaLivro(req.params.id);
+ res.status(200).json(livros[index]);
+});
+
+
+function buscaLivro(id) {
+ return livros.findIndex(livro => {
+     return livro.id === Number(id);
+ });
+}
+
 app.post("/livros",(req,res)=>{
  livros.push(req.body);
  res.status(201).send("livros Cadastrado com sucesso!");
+});
+
+app.put("/livros/:id",(req,res)=>{
+ const index = buscaLivro(req.params.id);
+ livros[index].titulo = req.body.titulo;
+ res.status(201).json(livros);
+});
+
+app.delete("/livros/:id",(req,res)=>{
+ const index = buscaLivro(req.params.id);
+ livros.splice(index,1);
+ res.status(200).send("livros deletado com sucesso!");
 });
 
 export default app;
